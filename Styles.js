39e5a5ed846c015fa18f9b61f3ed1e0b66fd590a
NartZx <151170135+NartZx@users.noreply.github.com>
2024-10-07 21:57:25 -0300

    Finally some progress. After discarding some conflicting files did a decent way of 3 js files into one "App.js"

---
+++ Styles.js
@@ -0,0 +1,24 @@
+import {StyleSheet} from 'react-native';
+
+
+const styles = StyleSheet.create({
+   container:{flex:1,
+              backgroundColor:'#fff',
+              alignItems:'center',
+              justifyContent:'center'},
+   
+   forminput:{borderColor:'red',
+              borderWidth:1,
+              borderRadius:10,
+              fontSize:22,
+              width:'100%',
+              padding:10,
+              margin:10}
+
+
+
+
+
+});
+
+export default styles;
