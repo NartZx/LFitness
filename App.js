48d9fa9463115007326dda7e7dbe1e4e2b390728
NartZx <151170135+NartZx@users.noreply.github.com>
2024-10-08 15:43:44 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,39 +1,11 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { useState } from 'react';
-import { createStackNavigator } from "@react-navigation/stack";
-import Login from './Login.js';
-import Home from './Home.js';
-const Stack = createStackNavigator();
+import { NavigationContainer } from '@react-navigation/native';
+import Routes from './Routes';
 
-const App = () => {
-  const [EstaLogado, setLogado] = useState(false);
 
-  return (
-    <NavigationContainer>
-      {EstaLogado ? (
-        
-              
-                    <Stack.Navigator >
-                      <Stack.Screen  name="Home" options={{ headerShown: true }} component={Home} initialParams={{ funcLogar: setLogado }} />
-                      
-                    </Stack.Navigator>
-                  
-             
-                
-              
-            
-          
-          
-        
-      ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={Login} initialParams={{ funcLogar: setLogado }} />
-          
-        </Stack.Navigator>
-      )}
-    </NavigationContainer>
-  );
-};
-
-export default App;
-
+export default function App() {
+	return (
+		<NavigationContainer>
+			<Routes />
+		</NavigationContainer>
+	);
+}
