3c6cc97687d214ba5cf71e6a1a23d781e3d2b01b
NartZx <151170135+NartZx@users.noreply.github.com>
2024-10-09 20:28:25 -0300

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Button} from 'react-native';
+import { View, Pressable,Text} from 'react-native';
 import styles from './Styles';
 
 
@@ -13,8 +13,19 @@ const Home = ({ navigation, route }) => {
 
   return (
     <View style={styles.container}>
+      <Pressable style={styles.formbutton} onPress={() => navigation.push("Consulta")}>
+        <Text>Consulta</Text> 
+        </Pressable>
+      <Pressable style={styles.formbutton} onPress={() => navigation.push("Avaliação Fisica")}>
+        <Text>{'Avalição Fisica'}</Text> 
+        </Pressable> 
+      <Pressable style={styles.formbutton} onPress={() => navigation.push("Aluno")}>
+        <Text>Aluno</Text> 
+        </Pressable>     
+      <Pressable  style={styles.formbutton} onPress={deslogar}> 
+         <Text>Logout</Text>
+         </Pressable>
       
-      <Button  title="Logout" onPress={deslogar} />
     </View>
   );
 };
