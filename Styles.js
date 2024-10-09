9f146e4058792fed4ecdc329362312a35d3f6202
NartZx <151170135+NartZx@users.noreply.github.com>
2024-10-09 20:27:03 -0300

    Update Styles.js

--- Styles.js
+++ Styles.js
@@ -13,7 +13,13 @@ const styles = StyleSheet.create({
               fontSize:22,
               width:'100%',
               padding:10,
-              margin:10}
+              margin:10},
+   formbutton:{backgroundColor:'red',
+               width:'80%',
+               margin:10,
+               padding:10,
+               borderRadius:10,
+               alignItems:'center',  }           
 
 
 
